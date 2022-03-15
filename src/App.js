1c58fadc801416f3f02cb3ec2027c0db38f48f77
Snikerso <42245963+Snikerso@users.noreply.github.com>
2022-03-15 13:04:13 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import './App.css';
 import styled, {css} from "styled-components"
+import { useState } from 'react';
 
 
 const Square = styled.div`
   width: 100px;
-  
   height: 100px;
   border: 10px solid red;
   background-color: black;
   
-  ${({cssIndex}) => cssIndex  && css` 
+  ${({cssIndex}) => cssIndex && css` 
      width: 200px;
   `}
 
 `
 
-function App() {
+const Button = styled.button`
+  width: 100px;
+  height: 100px;
+  background-color: yellow;
 
+
+`
+
+const useStateNasz = () => {
+  const [cssIndex, setCssIndex] = useState(1)
+
+  const handleChange = () =>{
+    setCssIndex(cssIndex + 1)
+  }
+
+  return [cssIndex, setCssIndex, handleChange]
+} 
+
+function App() {
+  const [zmiennaZwracana, FunkcjaZwracana, handleChange ] = useStateNasz()
 
   return (
     <div id='wrapper'>
-      <Square cssIndex={true}>1</Square>
-      <Square isBlack={true}>2</Square>
-      <Square >3</Square>
+      <Square cssIndex={zmiennaZwracana}></Square>
+      <Square cssIndex={zmiennaZwracana} isBlack={true}>2</Square>
+      <Square cssIndex={zmiennaZwracana}>3</Square>
+
+      <Button onClick={() => handleChange() }>{zmiennaZwracana}</Button>
     </div>
   );
 }
