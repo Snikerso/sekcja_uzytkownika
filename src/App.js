4c6ad1d635a153e6ec37d6b1e14e0e106cdd9fe0
Kanonashi <100380552+Kanonashi@users.noreply.github.com>
2022-04-05 19:43:50 +0200

    bebzon

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { useState } from "react";
 // template string``
 // string: "" ''
 const Head = styled.div`
-margin: 100px auto;
+  margin: 100px auto;
   position: relative;
-  width: 800px;
-  height: 800px;
-  background-color: red;
-  border: 30px solid black;
+  width: 300px;
+  height: 300px;
+  background-color: green;
+  border: 2px solid black;
   border-radius: 100%;
 `;
 
@@ -18,9 +18,9 @@ const Eye = styled.div`
   position: absolute;
   top: 80px;
   left: 80px;
-  width: 100px;
-  height: 80px;
-  background-color: brown;
+  width: 40px;
+  height: 40px;
+  background-color: black;
   border-radius: 500%;
   ${({ red }) =>
     red === true &&
@@ -28,12 +28,22 @@ const Eye = styled.div`
       background-color: red;
     `}
 `;
+const Body = styled.div`
+position: absolute;
+top: 180px;
+left: 180px;
+width: 140px;
+background-color: pink;
+border-radius: 50%`
 
 function App() {
   return (
     <div>
       <Head>
         <Eye red={false} />
+        <Body>
+          
+        </Body>
         <Eye red={true} />
       </Head>
     </div>
